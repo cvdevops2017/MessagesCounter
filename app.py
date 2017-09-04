441c9baf0e2d599fe009e35bd3c39bea726ba327
Haim Kabesa <kabesos1@gmail.com>
2017-09-04 17:17:25 +0300

    mongo client fix

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from pymongo import MongoClient
 import socket
 
 #Mongo Settings
-#client = MongoClient('172.17.0.2', 27017)
-client = MongoClient('db', 27017)
+client = MongoClient('localhost', 27017)
 db = client.test_database
 collection = db.test_collection
 posts = db.posts
